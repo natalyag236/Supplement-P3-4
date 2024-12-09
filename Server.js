e072e421f82c1eee952a11af084c67ddd7c47f22
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-08 21:09:35 -0500

    doc: added documentation to express server code

--- Server.js
+++ Server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-
+/**
+ * POST / endpoint
+ * This endpoint receives a JSON body, extracts the "content" field, 
+ * and sends it back in the response as JSON.
+ */
 app.post('/', (req, res) => {
   const content = req.body.content;
   res.json({ content: content });
